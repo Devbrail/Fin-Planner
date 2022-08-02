ed905026b17caf4dd40e6dd163ce7e183593bda5
Hemanth S <hemanths@live.com>
2022-08-03 00:00:34 +0530

    Fix the export with DateRange

--- lib/data/expense/datasources/expense_manager_data_source.dart
+++ lib/data/expense/datasources/expense_manager_data_source.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
+
 import '../model/expense.dart';
 
 abstract class ExpenseManagerDataSource {
   Future<void> addOrUpdateExpense(Expense expense);
   Future<List<Expense>> expenses();
+  Future<List<Expense>> filteredExpenses(DateTimeRange dateTimeRange);
   Future<void> clearExpenses();
   Future<void> clearExpense(int key);
 }
