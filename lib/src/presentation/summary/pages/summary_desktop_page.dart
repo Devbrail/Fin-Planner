d5cafdb271c5814f5716ffb6b7c19986635859d4
Hemanth <hemanths@live.com>
2023-01-19 22:11:04 +0530

    Fix import

--- lib/src/presentation/summary/pages/summary_desktop_page.dart
+++ lib/src/presentation/summary/pages/summary_desktop_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/enum/filter_budget.dart';
-import '../../home/widgets/welcome_widget.dart';
-import '../../settings/widgets/user_profile_widget.dart';
-import '../../widgets/color_palette.dart';
 import '../../widgets/filter_widget/filter_budget_widget.dart';
-import '../../widgets/paisa_search_bar.dart';
 import '../widgets/expense_history_widget.dart';
 import '../widgets/expense_total_widget.dart';
-import '../widgets/welcome_name_widget.dart';
 
 class SummaryDesktopPage extends StatelessWidget {
   const SummaryDesktopPage({
