4370c816c3bec03a0226fc70717f61e2cda1dc20
Hemanth <hemanths@live.com>
2023-03-28 21:17:48 +0530

    Bumped up version

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_displaymode/flutter_displaymode.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:get_it/get_it.dart';
-import 'package:workmanager/workmanager.dart';
 
 import 'src/app.dart';
 import 'src/di/di.dart';
-import 'src/di/module/data_module.dart';
 import 'src/di/module/work.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> main() async {
-  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
-  await initHive();
   await configInjector(getIt);
-  if (Platform.isAndroid) {
-    await FlutterDisplayMode.setHighRefreshRate();
-  }
-  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
   runApp(const PaisaApp());
 }
--- lib/src/di/di.dart
+++ lib/src/di/di.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter_displaymode/flutter_displaymode.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 import 'di.config.dart';
+import 'module/data_module.dart';
 
 @InjectableInit(
   asExtension: false,
@@ -13,6 +18,11 @@ Future<GetIt> configInjector(
   String? env,
   EnvironmentFilter? environmentFilter,
 }) async {
+  usePathUrlStrategy();
+  await initHive();
+  if (Platform.isAndroid) {
+    await FlutterDisplayMode.setHighRefreshRate();
+  }
   return init(
     getIt,
     environmentFilter: environmentFilter,
