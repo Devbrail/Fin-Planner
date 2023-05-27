bf0d24f329e46aa49c1e2cf53ef414d33b1d456a
Hemanth <hemanths@live.com>
2023-05-27 19:48:23 +0530

    Update on toggle change style

--- lib/src/presentation/accounts/pages/accounts_page.dart
+++ lib/src/presentation/accounts/pages/accounts_page.dart
@@ -4,9 +4,9 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import '../../../../main.dart';
 import '../../../core/common.dart';
+import '../../../core/enum/box_types.dart';
 import '../../../data/accounts/model/account_model.dart';
 import '../../../domain/account/entities/account.dart';
-import '../../settings/bloc/settings_controller.dart';
 import '../../widgets/paisa_empty_widget.dart';
 import 'horizontal/account_mobile_horizontal_page.dart';
 import 'vertical/account_mobile_vertical_page.dart';
@@ -16,26 +16,31 @@ class AccountsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final SettingsController settingsController = getIt.get();
     return Scaffold(
       key: const Key('accounts_mobile'),
-      body: ValueListenableBuilder<Box<AccountModel>>(
-        valueListenable: getIt.get<Box<AccountModel>>().listenable(),
-        builder: (_, value, __) {
-          final List<Account> accounts = value.toEntities();
-          if (accounts.isEmpty) {
-            return EmptyWidget(
-              icon: Icons.credit_card,
-              title: context.loc.emptyAccountMessageTitle,
-              description: context.loc.emptyAccountMessageSubTitle,
-            );
-          }
-          if (settingsController.get(userAccountsStyleKey,
-              defaultValue: false)) {
-            return AccountMobileHorizontalPage(accounts: accounts);
-          } else {
-            return AccountMobileVerticalPage(accounts: accounts);
-          }
+      body: ValueListenableBuilder<Box<dynamic>>(
+        valueListenable: getIt
+            .get<Box<dynamic>>(instanceName: BoxType.settings.name)
+            .listenable(keys: [userAccountsStyleKey]),
+        builder: (_, settings, __) {
+          return ValueListenableBuilder<Box<AccountModel>>(
+            valueListenable: getIt.get<Box<AccountModel>>().listenable(),
+            builder: (_, value, __) {
+              final List<Account> accounts = value.toEntities();
+              if (accounts.isEmpty) {
+                return EmptyWidget(
+                  icon: Icons.credit_card,
+                  title: context.loc.emptyAccountMessageTitle,
+                  description: context.loc.emptyAccountMessageSubTitle,
+                );
+              }
+              if (settings.get(userAccountsStyleKey, defaultValue: false)) {
+                return AccountMobileHorizontalPage(accounts: accounts);
+              } else {
+                return AccountMobileVerticalPage(accounts: accounts);
+              }
+            },
+          );
         },
       ),
     );
