cc80be6431e05980f2be3a57089400336b40968e
Hemanth <hemanths@live.com>
2023-06-24 00:29:45 +0530

    Code refactor

--- lib/src/data/currencies/repository/currencies_repository_impl.dart
+++ lib/src/data/currencies/repository/currencies_repository_impl.dart
@@ -2,15 +2,9 @@ import 'package:injectable/injectable.dart';
 
 import '../../../domain/currencies/repository/currencies_repository.dart';
 import '../models/country_model.dart';
-import '../models/currency_model.dart';
 
 @Singleton(as: CurrenciesRepository)
 class CurrencySelectorRepositoryImpl implements CurrenciesRepository {
-  @override
-  List<CurrencyModel> fetchCurrencies() {
-    return getLocales();
-  }
-
   @override
   List<CountryModel> fetchCountries() {
     return countryFromJson();
--- lib/src/domain/currencies/repository/currencies_repository.dart
+++ lib/src/domain/currencies/repository/currencies_repository.dart
@@ -1,7 +1,5 @@
 import '../../../data/currencies/models/country_model.dart';
-import '../../../data/currencies/models/currency_model.dart';
 
 abstract class CurrenciesRepository {
-  List<CurrencyModel> fetchCurrencies();
   List<CountryModel> fetchCountries();
 }
