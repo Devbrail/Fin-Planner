d99458a0365c9363a2f3c86e2d4e492bfa5fdbc5
Hemanth <hemanths@live.com>
2023-07-12 22:12:46 +0530

    Fix transaction name hides when scrolled

--- lib/src/presentation/transaction/widgets/transaction_name_widget.dart
+++ lib/src/presentation/transaction/widgets/transaction_name_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:paisa/src/core/enum/transaction_type.dart';
 
 import '../../../core/common.dart';
 import '../../widgets/paisa_text_field.dart';
@@ -19,31 +20,31 @@ class TransactionNameWidget extends StatelessWidget {
     return BlocBuilder<TransactionBloc, TransactionState>(
       buildWhen: (oldState, newState) => newState is ChangeTransactionTypeState,
       builder: (context, state) {
+        String hintName = TransactionType.expense.hintName(context);
         if (state is ChangeTransactionTypeState) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: PaisaTextFormField(
-              maxLines: 1,
-              controller: controller,
-              hintText: state.transactionType.hintName(context),
-              keyboardType: TextInputType.name,
-              inputFormatters: [
-                FilteringTextInputFormatter.singleLineFormatter,
-              ],
-              validator: (value) {
-                if (value!.isNotEmpty) {
-                  return null;
-                } else {
-                  return context.loc.validName;
-                }
-              },
-              onChanged: (value) =>
-                  BlocProvider.of<TransactionBloc>(context).expenseName = value,
-            ),
-          );
-        } else {
-          return const SizedBox.shrink();
+          hintName = state.transactionType.hintName(context);
         }
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: PaisaTextFormField(
+            maxLines: 1,
+            controller: controller,
+            hintText: hintName,
+            keyboardType: TextInputType.name,
+            inputFormatters: [
+              FilteringTextInputFormatter.singleLineFormatter,
+            ],
+            validator: (value) {
+              if (value!.isNotEmpty) {
+                return null;
+              } else {
+                return context.loc.validName;
+              }
+            },
+            onChanged: (value) =>
+                BlocProvider.of<TransactionBloc>(context).expenseName = value,
+          ),
+        );
       },
     );
   }
